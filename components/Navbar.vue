5df60a53acf6d93a910b29cb44a7e6ad7a4e4a3f
Jack Koskie <65452167+GoldenXLence@users.noreply.github.com>
2023-10-09 15:10:23 -0400

    feat!: upgrade to nuxt

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -2,7 +2,8 @@
   <nav class="navbar navbar-expand-md navbar-dark bg-dark sticky-top">
     <div class="container">
       <a href="#top" class="navbar-brand"
-        ><span class="fw-lighter">Jack</span><span class="fw-bold">Koskie</span></a
+        ><span class="fw-lighter">Jack</span
+        ><span class="fw-bold">Koskie</span></a
       >
       <button
         class="navbar-toggler"
@@ -17,8 +18,12 @@
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
-          <li class="nav-item"><a href="#top" class="nav-link">Home</a></li>
-          <li class="nav-item"><a href="#contact" class="nav-link">Contact Me</a></li>
+          <li class="nav-item">
+            <NuxtLink to="#top" class="nav-link">Home</NuxtLink>
+          </li>
+          <li class="nav-item">
+            <NuxtLink to="#contact" class="nav-link">Contact Me</NuxtLink>
+          </li>
         </ul>
       </div>
     </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,19 @@
-<script setup></script>
+<script setup lang="ts">
+const route = useRoute();
+</script>
 
 <template>
   <main>
     <section id="hero">
       <div class="container">
-        <div class="d-flex flex-column justify-content-center pt-5 pt-lg-0" id="hero-text">
+        <div
+          class="d-flex flex-column justify-content-center pt-5 pt-lg-0"
+          id="hero-text"
+        >
           <h1 class="text-white pt-5 pt-lg-0">Hey, I'm Jack!</h1>
           <h3 class="text-white fw-lighter col-md-6">
-            A highschool student, web developer, drone photographer, musician, and more!
+            A highschool student, web developer, drone photographer, musician,
+            and more!
           </h3>
         </div>
       </div>
@@ -20,18 +26,23 @@
           Have any questions? Do you want to hire me? Just want to chat?
         </h3>
         <p>
-          The best way to reach me is by email. I do my best to reply to everything I get within
-          24-48 hours. Please dont hesitate to reach out!
+          The best way to reach me is by email. I do my best to reply to
+          everything I get within 24-48 hours. Please dont hesitate to reach
+          out!
         </p>
-        <p class="">My email is <a href="mailto:jack@koskie.ca">jack@koskie.ca</a>.</p>
+        <p class="">
+          My email is <a href="mailto:jack@koskie.ca">jack@koskie.ca</a>.
+        </p>
 
         <span class="d-flex flex-row gap-3 fs-2 mb-3">
-          <a href="https://instagram.com/koskie.jack" class="link-dark"
+          <NuxtLink to="https://instagram.com/koskie.jack" class="link-dark"
             ><i class="bi bi-instagram"></i
-          ></a>
-          <a href="https://www.facebook.com/profile.php?id=61551088465175" class="link-dark"
+          ></NuxtLink>
+          <NuxtLink
+            to="https://www.facebook.com/profile.php?id=61551088465175"
+            class="link-dark"
             ><i class="bi bi-facebook"></i
-          ></a>
+          ></NuxtLink>
         </span>
       </div>
     </section>
@@ -41,7 +52,7 @@
 <style>
 #hero {
   background-image: linear-gradient(rgba(0, 0, 0, 0.473), rgba(0, 0, 0, 0.473)),
-    url('../assets/hero.jpg');
+    url('/hero.jpg');
   background-size: cover;
   background-position: center;
   height: 100vh;
