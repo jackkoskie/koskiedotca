c44f3352df114266a9bc7e1864e5416944458917
Jack Koskie <65452167+jackkoskie@users.noreply.github.com>
2024-10-30 23:31:29 -0400

    Update now.vue

--- pages/now.vue
+++ pages/now.vue
@@ -9,16 +9,12 @@
         </h5>
       <hr>
       <p>Hello! This is my <a href="https://nownownow.com">/now page</a>. This is a place where I will update with what is going on in my life right now. It will be less of a real time update feed and more of a, "oh, so that's what he's up to..." kinda thing.</p>
+
+      <p>Recently I've been trying to be more mindful about how I spend my time and one of the ways I look to acheive this is through the use of a pocket notebook and some mild journaling. For my notebok, I picked up a 3 pack of the <a href="https://fieldnotesbrand.com/">Field Notes</a> note books, specificaly the <a href="https://fieldnotesbrand.com/products/heartland">Heartland pack.</a></p>
+
       <h6>Current / Recent Projects:</h6>
       <ul>
         <li>CZQM Website Redesign</li>
-        <li>Configure NeoVim to be used as my main text/code editor and for taking notes (<a href="https://github.com/jackkoskie/kickstart.nvim">GitHub</a>)</li>
-      </ul>
-
-      <h6>Projects To Do:</h6>
-      <ul>
-        <li>Redo <NuxtLink to="/" class="">koskie.ca</NuxtLink> to use Svelte kit</li>
-        <li>Add a blog-ish thing to <NuxtLink to="/" class="">koskie.ca</NuxtLink></li>
       </ul>
 
       <h6>School:</h6>
