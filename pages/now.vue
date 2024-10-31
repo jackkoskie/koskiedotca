dbc5911ed26879f00f9f728cb9c93c67f148c08c
Jack Koskie <65452167+jackkoskie@users.noreply.github.com>
2024-10-30 23:38:07 -0400

    update dependencies

--- pages/now.vue
+++ pages/now.vue
@@ -4,27 +4,34 @@
     <section id="contact">
       <div class="container py-5">
         <h1>What am I up to right now?</h1>
+
+        <p>Hello! This is my <a href="https://nownownow.com">/now page</a>. This is a place where I will update with what is going on in my life right now. It will be less of a real time update feed and more of a, "oh, so that's what he's up to..." kinda thing.</p>
+        
+        <h5 class="fw-normal col-md-4 my-4">October 2024</h5>
+        <hr>
+
+        <p>Recently I've been trying to be more mindful about how I spend my time and one of the ways I look to acheive this is through the use of a pocket notebook and some mild journaling. For my notebok, I picked up a 3 pack of the <a href="https://fieldnotesbrand.com/" target="_blank">Field Notes</a> note books, specificaly the <a href="https://fieldnotesbrand.com/products/heartland" target="_blank">Heartland pack.</a></p>
+
+
         <h5 class="fw-normal col-md-4 my-4">
         August 2024
         </h5>
-      <hr>
-      <p>Hello! This is my <a href="https://nownownow.com">/now page</a>. This is a place where I will update with what is going on in my life right now. It will be less of a real time update feed and more of a, "oh, so that's what he's up to..." kinda thing.</p>
+        <hr>
 
-      <p>Recently I've been trying to be more mindful about how I spend my time and one of the ways I look to acheive this is through the use of a pocket notebook and some mild journaling. For my notebok, I picked up a 3 pack of the <a href="https://fieldnotesbrand.com/">Field Notes</a> note books, specificaly the <a href="https://fieldnotesbrand.com/products/heartland">Heartland pack.</a></p>
 
-      <h6>Current / Recent Projects:</h6>
-      <ul>
-        <li>CZQM Website Redesign</li>
-      </ul>
+        <h6>Current / Recent Projects:</h6>
+        <ul>
+          <li>CZQM Website Redesign</li>
+        </ul>
 
-      <h6>School:</h6>
-      <ul>
-        <li>Grade 12</li>
-        <li>3 Math Courses</li>
-        <li>Physics</li>
-        <li>Chemistry</li>
-        <li>Computer Science</li>
-      </ul>
+        <h6>School:</h6>
+        <ul>
+          <li>Grade 12</li>
+          <li>3 Math Courses</li>
+          <li>Physics</li>
+          <li>Chemistry</li>
+          <li>Computer Science</li>
+        </ul>
 
       </div>
     </section>
