8e129ec34732ac41075a14373545f931a86f7255
Jack Koskie <65452167+jackkoskie@users.noreply.github.com>
2024-12-07 13:19:31 -0500

    feat: move mobile nav to sidebar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,55 +3,59 @@
 	let { children } = $props();
 </script>
 
-<nav class="navbar bg-base-300">
-	<div class="navbar-start">
-		<div class="dropdown">
-			<div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					class="h-5 w-5"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke="currentColor"
-				>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						d="M4 6h16M4 12h8m-8 6h16"
-					/>
-				</svg>
+<div class="drawer">
+	<input id="nav-sidebar" type="checkbox" class="drawer-toggle" />
+	<div class="drawer-content flex flex-col">
+		<!-- Navbar -->
+		<div class="navbar w-full bg-base-300">
+			<div class="flex-none lg:hidden">
+				<label for="nav-sidebar" aria-label="open sidebar" class="btn btn-square btn-ghost">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						class="inline-block h-6 w-6 stroke-current"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M4 6h16M4 12h16M4 18h16"
+						></path>
+					</svg>
+				</label>
 			</div>
-			<ul
-				tabindex="0"
-				class="menu dropdown-content menu-md z-[1] mt-3 w-52 rounded-box bg-base-300 p-2 shadow"
-			>
-				<li><a href="/">Home</a></li>
-				<li><a href="https://github.com/jackkoskie">GitHub</a></li>
-				<li><a href="/utilities">Utilities</a></li>
-				<li><a href="/contact">Contact Me</a></li>
-			</ul>
+			<a href="/" class="btn btn-ghost text-xl">Jack Koskie</a>
+			<div class="hidden lg:flex">
+				<ul class="menu menu-horizontal px-1">
+					<li><a href="/">Home</a></li>
+					<li><a href="https://github.com/jackkoskie">GitHub</a></li>
+					<li><a href="/utilities">Utilities</a></li>
+					<li><a href="/contact">Contact Me</a></li>
+				</ul>
+			</div>
 		</div>
-		<a class="btn btn-ghost text-xl">Jack Koskie</a>
+
+		<main>
+			{@render children()}
+		</main>
+
+		<footer class="footer footer-center bg-base-300 p-4 text-base-content">
+			<aside>
+				<p>
+					Copyright © {new Date().getFullYear()} -
+					<a href="https://koskie.ca" class="hover:link">Made by Jack Koskie</a>
+				</p>
+			</aside>
+		</footer>
 	</div>
-	<div class="navbar-center"></div>
-	<div class="navbar-end hidden lg:flex">
-		<ul class="menu menu-horizontal px-1">
+	<div class="drawer-side">
+		<label for="nav-sidebar" aria-label="close sidebar" class="drawer-overlay"></label>
+		<ul class="menu min-h-full w-80 bg-base-200 p-4">
 			<li><a href="/">Home</a></li>
 			<li><a href="https://github.com/jackkoskie">GitHub</a></li>
 			<li><a href="/utilities">Utilities</a></li>
 			<li><a href="/contact">Contact Me</a></li>
 		</ul>
 	</div>
-</nav>
-
-{@render children()}
-
-<footer class="footer footer-center bg-base-300 p-4 text-base-content">
-	<aside>
-		<p>
-			Copyright © {new Date().getFullYear()} -
-			<a href="https://koskie.ca" class="hover:link">Made by Jack Koskie</a>
-		</p>
-	</aside>
-</footer>
+</div>
