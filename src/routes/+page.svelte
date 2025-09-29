107f812521fc4bb50043c5f2717c23c38012e9de
Jack Koskie <65452167+jackkoskie@users.noreply.github.com>
2025-09-29 12:12:50 -0700

    chore: initialize project from scratch

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,7 @@
 <script lang="ts">
-	import type { PageData } from './$types';
-	import SEO from '$lib/components/SEO.svelte';
+	import type { PageProps } from './$types';
 
-	let { data }: { data: PageData } = $props();
+	let { data }: PageProps = $props();
 </script>
 
-<SEO
-	siteName="Jack Koskie"
-	description="Hey! My name is Jack and this is my personal site."
-	url="https://koskie.ca"
-/>
-
-<div class="hero min-h-[calc(100vh-4.22rem)] bg-base-200">
-	<div class="hero-content text-center">
-		<div class="max-w-md">
-			<h1 class="text-5xl font-bold">Hey there!</h1>
-			<p class="py-6">My name is Jack, I'm a web developer, musician, and student.</p>
-		</div>
-	</div>
-</div>
-
-<style>
-	.hero {
-		background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)),
-			url('$lib/images/hero.jpg');
-		background-size: cover;
-		background-position: center;
-		position: relative;
-	}
-</style>
+<p>Welcome to SvelteKit</p>
