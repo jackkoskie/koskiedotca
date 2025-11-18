c915b01d0117b2a8dde1955deeec8d0265db0a02
Jack Koskie <65452167+jackkoskie@users.noreply.github.com>
2025-11-17 17:54:20 -0800

    chore: reset back to basics

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,2 @@
-<script lang="ts">
-	import type { PageProps } from './$types';
-
-	let { data }: PageProps = $props();
-</script>
-
-<p>Welcome to SvelteKit</p>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
